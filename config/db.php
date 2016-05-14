e108856639f2dcfbf8c5a1d5ca968020674fb509
Bota Laszlo <bota.laszlo.gabor@outlook.com>
2016-05-14 12:39:16 +0200

    Config updated. Gitignore did not ignore the config file.

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=basic',
     'username' => 'root',
-    'password' => 'Yupi45',
+    'password' => '',
     'charset' => 'utf8',
 ];
